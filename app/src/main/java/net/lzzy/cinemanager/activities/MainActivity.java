a944659938e13c7d360b02fcdd65d7b8020e7b45
Liangsimin <320366897@qq.com>
2019-03-26 17:42:12 +0800

    创建Fragment并托管，2019年3月26日第10节

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,11 +11,15 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.fragments.CinemasFragment;
+import net.lzzy.cinemanager.fragments.OrdersFragment;
 
 /**
  * @author Administrator
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+   private FragmentManager manager = getSupportFragmentManager();
+
     private View layoutMenu;
     private SearchView search;
     private TextView tvTitle;
@@ -26,7 +31,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         setTitleMenu();
     }
-
     private void setTitleMenu() {
         layoutMenu = findViewById(R.id.bar_title_layout_menu);
         layoutMenu.setVisibility(View.GONE);
@@ -45,35 +49,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.bar_title_tv_exit).setOnClickListener(v -> System.exit(0));
 
     }
-
     @Override
     public void onClick(View v) {
+        layoutMenu.setVisibility(View.GONE);
         switch (v.getId()) {
             case R.id.bar_title_img_menu:
-
                 break;
             case R.id.bar_title_tv_exit:
-
                 break;
 
             case R.id.bar_title_tv_add_cinema:
                 tvTitle.setText(R.string.bar_title_menu_add_cinema);
                 break;
             case R.id.bar_title_tv_view_cinema:
-                tvTitle.setText(R.string.bar_title_menu_cinema);
-
+                tvTitle.setText("影院列表");
+                manager.beginTransaction()
+                        .replace(R.id.forever_container,new CinemasFragment())
+                        .commit();
                 finish();
                 break;
             case R.id.bar_title_tv_add_order:
-                tvTitle.setText(R.string.bar_title_menu_add_orders);
-                layoutMenu.setVisibility(View.GONE);
-                layoutAddOrder.setVisibility(View.VISIBLE);
                 break;
             case R.id.bar_title_tv_view_order:
+                tvTitle.setText("我的订单");
+                manager.beginTransaction()
+                        .replace(R.id.fragment_cintainer,new OrdersFragment())
+                        .commit();
                 tvTitle.setText(R.string.bar_title_menu_orders);
                 layoutMenu.setVisibility(View.GONE);
                 layoutAddOrder.setVisibility(View.GONE);
                 break;
+
 
             default:
                 break;
